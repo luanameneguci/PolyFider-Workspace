499ce4fa29e15f4cbb828fdac4ce7fa30ae76acb
AlexCraveiro <ajscraveiro@hotmail.com>
2023-12-15 12:49:52 +0000

    Aula 15/12

---
+++ experiencia.js
@@ -0,0 +1,26 @@
+function Afunda1(){
+    document.getElementById("copo1").style.display="none";
+    document.getElementById("copo1afunda").style.display="block";
+}
+function Flutua1(){
+    document.getElementById("copo1").style.display="none";
+    document.getElementById("copo1flutua").style.display="block";
+    document.getElementById("parte2").style.display="block";
+}
+function Afunda2(){
+    document.getElementById("copo2").style.display="none";
+    document.getElementById("copo2afunda").style.display="block";
+}
+function Flutua2(){
+    document.getElementById("copo2").style.display="none";
+    document.getElementById("copo2flutua").style.display="block";
+    document.getElementById("parte3").style.display="block"
+}
+function Afunda3(){
+    document.getElementById("copo3").style.display="none";
+    document.getElementById("copo3afunda").style.dispplay="block";
+}
+function Flutua3(){
+    document.getElementById("copo3").style.display="none";
+    document.getElementById("copo3flutua").style.display="block";
+}
