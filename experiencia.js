e479cb0d374613dd4e544a2ebf347c443baa2176
AlexCraveiro <ajscraveiro@hotmail.com>
2023-12-19 18:14:06 +0000

    Aula PI2 19/12

--- experiencia.js
+++ experiencia.js
@@ -1,6 +1,6 @@
 function Afunda1(){
-    document.getElementById("copo1").style.display="none";
-    document.getElementById("copo1afunda").style.display="block";
+    document.getElementById("parte1").style.display="none";
+    document.getElementById("resultado1").style.display="block";
 }
 function Flutua1(){
     document.getElementById("copo1").style.display="none";
@@ -8,8 +8,9 @@ function Flutua1(){
     document.getElementById("parte2").style.display="block";
 }
 function Afunda2(){
-    document.getElementById("copo2").style.display="none";
-    document.getElementById("copo2afunda").style.display="block";
+    document.getElementById("parte1").style.display="none";
+    document.getElementById("parte2").style.display="none";
+    document.getElementById("resultado2").style.display="block";
 }
 function Flutua2(){
     document.getElementById("copo2").style.display="none";
@@ -17,18 +18,27 @@ function Flutua2(){
     document.getElementById("parte3").style.display="block"
 }
 function Afunda3(){
-    document.getElementById("copo3").style.display="none";
-    document.getElementById("copo3afunda").style.display="block";
+    document.getElementById("parte1").style.display="none";
+    document.getElementById("parte2").style.display="none";
+    document.getElementById("parte3").style.display="none";
+    document.getElementById("resultado3").style.display="block";
 }
 function Flutua3(){
     document.getElementById("copo3").style.display="none";
     document.getElementById("copo3flutua").style.display="block";
+    document.getElementById("parte4").style.display="block";
 }
 function Afunda4(){
-    document.getElementById("copo4").style.display="none";
-    document.getElementById("copo4afunda").style.display="block";
+    document.getElementById("parte1").style.display="none";
+    document.getElementById("parte2").style.display="none";
+    document.getElementById("parte3").style.display="none";
+    document.getElementById("parte4").style.display="none";
+    document.getElementById("resultado4").style.display="block";
 }
 function Flutua4(){
+    document.getElementById("parte1").style.display="none";
+    document.getElementById("parte2").style.display="none";
+    document.getElementById("parte3").style.display="none";
     document.getElementById("copo4").style.display="none";
     document.getElementById("copo4flutua").style.display="block";
 }
