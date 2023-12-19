995a783dd455e4ac987496a3e82a4feba4d7d33a
AlexCraveiro <ajscraveiro@hotmail.com>
2023-12-19 10:03:59 +0000

    19/12 parte 1

--- experiencia.js
+++ experiencia.js
@@ -18,9 +18,17 @@ function Flutua2(){
 }
 function Afunda3(){
     document.getElementById("copo3").style.display="none";
-    document.getElementById("copo3afunda").style.dispplay="block";
+    document.getElementById("copo3afunda").style.display="block";
 }
 function Flutua3(){
     document.getElementById("copo3").style.display="none";
     document.getElementById("copo3flutua").style.display="block";
+}
+function Afunda4(){
+    document.getElementById("copo4").style.display="none";
+    document.getElementById("copo4afunda").style.display="block";
+}
+function Flutua4(){
+    document.getElementById("copo4").style.display="none";
+    document.getElementById("copo4flutua").style.display="block";
 }
