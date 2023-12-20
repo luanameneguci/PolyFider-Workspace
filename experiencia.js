a1eaab2aeb9b3f32ff31d2235f87dc4f6735a085
AlexCraveiro <ajscraveiro@hotmail.com>
2023-12-20 15:50:42 +0000

    experiencia codigo

--- experiencia.js
+++ experiencia.js
@@ -1,32 +1,41 @@
 function Afunda1(){
     document.getElementById("parte1").style.display="none";
     document.getElementById("resultado1").style.display="block";
+    document.getElementById("teste1").style.display="none";
 }
 function Flutua1(){
     document.getElementById("copo1").style.display="none";
     document.getElementById("copo1flutua").style.display="block";
     document.getElementById("parte2").style.display="block";
+    document.getElementById("teste1").style.display="none";
+    document.getElementById("teste2").style.display="block";
 }
 function Afunda2(){
     document.getElementById("parte1").style.display="none";
     document.getElementById("parte2").style.display="none";
     document.getElementById("resultado2").style.display="block";
+    document.getElementById("teste2").style.display="none";
 }
 function Flutua2(){
     document.getElementById("copo2").style.display="none";
     document.getElementById("copo2flutua").style.display="block";
-    document.getElementById("parte3").style.display="block"
+    document.getElementById("parte3").style.display="block";
+    document.getElementById("teste2").style.display="none";
+    document.getElementById("teste3").style.display="block";
 }
 function Afunda3(){
     document.getElementById("parte1").style.display="none";
     document.getElementById("parte2").style.display="none";
     document.getElementById("parte3").style.display="none";
     document.getElementById("resultado3").style.display="block";
+    document.getElementById("teste3").style.display="none";
 }
 function Flutua3(){
     document.getElementById("copo3").style.display="none";
     document.getElementById("copo3flutua").style.display="block";
     document.getElementById("parte4").style.display="block";
+    document.getElementById("teste3").style.display="none";
+    document.getElementById("teste4").style.display="block";
 }
 function Afunda4(){
     document.getElementById("parte1").style.display="none";
@@ -34,6 +43,7 @@ function Afunda4(){
     document.getElementById("parte3").style.display="none";
     document.getElementById("parte4").style.display="none";
     document.getElementById("resultado4").style.display="block";
+    document.getElementById("teste4").style.display="none";
 }
 function Flutua4(){
     document.getElementById("parte1").style.display="none";
@@ -41,4 +51,5 @@ function Flutua4(){
     document.getElementById("parte3").style.display="none";
     document.getElementById("copo4").style.display="none";
     document.getElementById("copo4flutua").style.display="block";
+    document.getElementById("teste4").style.display="none";
 }
