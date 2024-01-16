02bfbc9c261cd97704c5576262085858eb840a6f
AlexCraveiro <ajscraveiro@hotmail.com>
2024-01-16 19:47:57 +0000

    onclick desaparecer mobile teste

--- mobile/java.js
+++ mobile/java.js
@@ -1,6 +1,12 @@
+/* Aplica ou remove a classe de css que faz rodar */
 function fazrodar(div_clicada){
     if(div_clicada.classList.contains("fazrodar"))
         div_clicada.classList.remove("fazrodar");
     else
         div_clicada.classList.add("fazrodar");
+}
+
+/* Esconde a imagem clicada*/
+function fazdesaparecer(elemento) {
+   elemento.style.display = 'none';
 }
