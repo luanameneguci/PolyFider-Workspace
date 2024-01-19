b696fbf242a9f6874816678f77a7f4085b929249
AlexCraveiro <ajscraveiro@hotmail.com>
2024-01-19 17:49:26 +0000

    Versão final do projeto

--- Desktop/java.js
+++ Desktop/java.js
@@ -2,3 +2,11 @@
 function fazdesaparecer(elemento) {
     elemento.style.display = 'none';
  }
+
+ function mostrarautores(){
+    if(document.getElementById("autores").style.display=="none")
+    {
+        document.getElementById("autores").style.display="block";
+    }else
+    document.getElementById("autores").style.display="none";
+}
--- mobile/java.js
+++ mobile/java.js
@@ -10,3 +10,11 @@ function fazrodar(div_clicada){
 function fazdesaparecer(elemento) {
    elemento.style.display = 'none';
 }
+
+function mostrarautores(){
+    if(document.getElementById("autores").style.display=="none")
+    {
+        document.getElementById("autores").style.display="block";
+    }else
+    document.getElementById("autores").style.display="none";
+}
